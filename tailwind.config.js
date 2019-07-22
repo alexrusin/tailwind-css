daab8d5b997654f1fd2ea9b3aa4d2785188ec8ea
Alex Rusin <alexrusin_2000@yahoo.com>
2019-07-21 18:51:41 -0700

    intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'brand-blue': '#394a66'
+      },
+      spacing: {
+        '72': '18rem'
+      }
+    }
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active']
+  },
+  plugins: []
+}
